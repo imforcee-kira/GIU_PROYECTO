633d167a2179309b0ca7a98a1ba6ce8803534a53
eze <eze@10.20.49.133>
2025-10-10 09:21:49 -0300

    CODIGO SOLUCIONADO

--- src/main/java/CapaPersistencia/PersistenciaPersona.java
+++ src/main/java/CapaPersistencia/PersistenciaPersona.java
@@ -85,5 +85,9 @@ public class PersistenciaPersona {
         return perso;
 
     }
+
+    public void eliminarPersona(String ci) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
  }
 
